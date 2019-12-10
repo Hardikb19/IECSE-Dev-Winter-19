cbd96e997e4927963dc9e2808fbeb4742fa09e7c
RUCHIR <ruchirrishi@gmail.com>
2019-12-11 02:03:28 +0530

    four rows of black stuff

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -19,24 +19,137 @@ class MyApp extends StatelessWidget{
           children: <Widget>[
             
             Container(
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(10),
+              color: Colors.black87,
               child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Image.asset('assets/cicada.png'),
-                  Text('Cicada-29/03/2019'),
-                  Text('Online')
+                  
+                  Image.asset(
+                    'assets/cicada.png',
+                    height:80.0,
+                    width:80.0,
+                  ),
+                  Text(
+                    'Cicada-29/03/2019',
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize:15,
+                    ),
+                  ),
+                  Text(
+                    'Online',
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize:15,
+                    ),
+                  ),
                 ],
               ),
             ),
            
             Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              color: Colors.black87,
               child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  Image.asset('assets/oc.png'),
-                  Text('Online Coding - 30/03/2019'),
-                  Text('Online')
+                  Image.asset(
+                    'assets/oc.png',
+                    height:80.0,
+                    width:80.0,
+                  ),
+                  Text(
+                    'Online Coding - 30/03/2019',
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    'Online',
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize:15,
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(10),
+              color: Colors.black87,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  
+                  Image.asset(
+                    'assets/gambit.png',
+                    height:80.0,
+                    width:80.0,
+                  ),
+                  Text(
+                    'Gambit-28/03/19',
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize:15,
+                    ),
+                  ),
+                  Text(
+                    'Online',
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize:15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10.0),
+              margin: EdgeInsets.all(10),
+              color: Colors.black87,
+              child:Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  
+                  Image.asset(
+                    'assets/hawkeye.png',
+                    height:80.0,
+                    width:80.0,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        'Hawkeye',
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontSize:15,
+                        ),
+                      ),
+                      Text(
+                        '28/03/2019',
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontSize:15,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'Online',
+                    style: TextStyle(
+                      color:Colors.white,
+                      fontSize:15,
+                    ),
+                  ),
+                ],
+              ),
+            ),
 
           ],
         ),
